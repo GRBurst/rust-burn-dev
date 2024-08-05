117ebcf9b09b8e809d3bf85411d988ec75c21a3f
GRBurst <GRBurst@protonmail.com>
2024-08-05 17:35:46 +0200

    replace devbox with flake for gpu support
    Signed-off-by: GRBurst <GRBurst@protonmail.com>

---
+++ shell.nix
@@ -0,0 +1,47 @@
+{ pkgs ? import <nixpkgs> { } }:
+let
+  pname = "burn";
+in
+pkgs.mkShell {
+
+  nativeBuildInputs = with pkgs; [
+    pkg-config
+    fontconfig
+  ];
+
+  buildInputs = with pkgs; [
+    rustup
+
+    vulkan-headers
+    vulkan-loader
+    vulkan-tools
+    vulkan-validation-layers
+
+    xorg.libX11
+    xorg.libXcursor
+    xorg.libXrandr
+    xorg.libXi
+
+    libxkbcommon
+
+    shaderc
+    glslang
+
+    blender
+  ];
+
+  shellHook = ''
+    echo "--- Welcome to ${pname}! ---"
+    projectDir=$PWD
+    rustupHomeDir="$projectDir/.rustup"
+    mkdir -p "$rustupHomeDir"
+
+    export RUSTUP_HOME="$rustupHomeDir"
+    export LIBRARY_PATH="$LIBRARY_PATH:$projectDir/nix/profile/default/lib"
+
+    export VK_LAYER_PATH="${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d"
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/.nix-profile/lib/:${pkgs.vulkan-loader}/lib:${pkgs.libxkbcommon}/lib:${pkgs.vulkan-validation-layers}/lib"
+
+    cargo fetch
+  '';
+}
